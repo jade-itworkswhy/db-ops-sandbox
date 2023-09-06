12a666a3c5c59d78415754fd408cdf544b9efa34
jonghyeon <jonghyeonpark.work@gmail.com>
2023-09-06 20:58:49 +0900

    feat: db connection

--- mongodb-transaction-sandbox/src/app.module.ts
+++ mongodb-transaction-sandbox/src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        /**
+         * Environment
+         */
+        NODE_ENV: Joi.string()
+          .valid('development', 'production')
+          .default('development'),
+        /**
+         * Mongodb connection string
+         */
+        DATABASE_URL: Joi.string().required(),
+      }),
+    }),
+    MongooseModule.forRoot(process.env.DATABASE_URL),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
